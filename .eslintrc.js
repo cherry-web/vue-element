e2d2c1d3c6d749f8c52b7c350893676423aae98e
cherry-web <1947351521@qq.com>
2021-06-17 11:38:16 +0800

    删除英文

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,16 +14,18 @@ module.exports = {
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
-    "vue/max-attributes-per-line": [2, {
-      "singleline": 10,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
-      }
-    }],
+    // "vue/max-attributes-per-line": [2, {
+    //   "singleline": 10,
+    //   "multiline": {
+    //     "max": 1,
+    //     "allowFirstLine": false
+    //   }
+    // }],
+    "vue/max-attributes-per-line": ["off"],
     "vue/singleline-html-element-content-newline": "off",
     "vue/multiline-html-element-content-newline":"off",
-    "vue/name-property-casing": ["error", "PascalCase"],
+    // "vue/name-property-casing": ["error", "PascalCase"],
+    "vue/name-property-casing": ["off"],
     "vue/no-v-html": "off",
     'accessor-pairs': 2,
     'arrow-spacing': [2, {
